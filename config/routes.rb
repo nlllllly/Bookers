321fe369af42a114053cc7a7a3e8e968fb42d990
vagrant <vagrant@localhost.localdomain>
2020-06-17 06:52:28 +0000

    [update]Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Viewの「root.html.erb」をルートに指定
   root to: 'books#root'
+  # REST 複数ルーティングの指定
   resources :books
 
 end
